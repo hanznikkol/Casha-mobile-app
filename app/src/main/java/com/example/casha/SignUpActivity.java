ff78a3d8e968c3266dab98011b48fea516ff8932
Hanz <hanznikkolmaas@gmail.com>
2023-11-01 14:44:12 +0800

    Update Commit 1

--- app/src/main/java/com/example/casha/SignUpActivity.java
+++ app/src/main/java/com/example/casha/SignUpActivity.java
@@ -2,13 +2,26 @@ package com.example.casha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    ImageButton btnBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        btnBackButton = findViewById(R.id.imgBtnBack);
+
+        btnBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
